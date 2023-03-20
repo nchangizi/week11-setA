8e8ec85336d185ebcd21939534549c40716c7cf8
Neda <nedachangizi@gmail.com>
2023-03-20 14:21:19 -0700

    funciton expression

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
-function greet(name) {
+// function greet(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+// greet("Neda");
+let greet = function(name) {
   console.log(`Hello ${name}`);
 }
 
 greet("Neda");
+greet("John")
