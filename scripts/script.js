9a6134716d1f5c2fd2648240ba4cbc32b094bcf0
Neda <nedachangizi@gmail.com>
2023-03-20 14:48:05 -0700

    area calc

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-function greet(name, time = "day") {
-  console.log(`Good ${time} ${name}`);
-}
+// function greet(name, time = "day") {
+//   console.log(`Good ${time} ${name}`);
+// }
 
-greet("Neda", "night", "sandwich");
+// greet("Neda", "night", "sandwich");
 
 
 // let greet = function(name) {
@@ -16,3 +16,16 @@ greet("Neda", "night", "sandwich");
 // };
 
 // greet("Neda");
+
+// Write a function that takes one parameter as radius of a circle and return the circles area. 
+function circleAreaCalculation (radius)
+{
+    let area = Math.PI * radius * radius;
+    return area;
+}
+// Prompt user to enter a value.
+let userValue = prompt("Enter a number");
+// Call the area calculating function with the value entered by user. 
+let result = circleAreaCalculation(userValue);
+// Show an alert message with both values e.g. “The area of a circle with radius 2 is 12.56”
+alert(`The area of a circle with radius ${userValue} is ${result.toFixed(2)}`)
