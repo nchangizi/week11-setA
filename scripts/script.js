847e7b96a78402d2cf348b2b525d9796e21e1447
Neda <nedachangizi@gmail.com>
2023-03-20 14:34:22 -0700

    default parameter

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
-// function greet(name) {
-//   console.log(`Hello ${name}`);
-// }
+function greet(name, time = "day") {
+  console.log(`Good ${time} ${name}`);
+}
 
-// greet("Neda");
+greet("Neda", "night", "sandwich");
+
+
 // let greet = function(name) {
 //   console.log(`Hello ${name}`);
 // }
 
 // greet("Neda");
 // greet("John")
-let greet = (name) => {
-  console.log(`Hello ${name}`);
-};
+// let greet = (name) => {
+//   console.log(`Hello ${name}`);
+// };
 
-greet("Neda");
+// greet("Neda");
