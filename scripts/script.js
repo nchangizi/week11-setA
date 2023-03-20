483deba31f1f1e79f9a881f51298568369167e0a
Neda <nedachangizi@gmail.com>
2023-03-20 14:49:42 -0700

    update area calc function with isNan check

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,6 @@
 
 // greet("Neda", "night", "sandwich");
 
-
 // let greet = function(name) {
 //   console.log(`Hello ${name}`);
 // }
@@ -17,15 +16,19 @@
 
 // greet("Neda");
 
-// Write a function that takes one parameter as radius of a circle and return the circles area. 
-function circleAreaCalculation (radius)
-{
+// Write a function that takes one parameter as radius of a circle and return the circles area.
+function circleAreaCalculation(radius) {
+  // check if radius is not a number and show an alert
+  if (isNaN(radius)) {
+    alert("This is not a string");
+  } else {
     let area = Math.PI * radius * radius;
     return area;
+  }
 }
 // Prompt user to enter a value.
 let userValue = prompt("Enter a number");
-// Call the area calculating function with the value entered by user. 
+// Call the area calculating function with the value entered by user.
 let result = circleAreaCalculation(userValue);
 // Show an alert message with both values e.g. “The area of a circle with radius 2 is 12.56”
-alert(`The area of a circle with radius ${userValue} is ${result.toFixed(2)}`)
+alert(`The area of a circle with radius ${userValue} is ${result.toFixed(2)}`);
