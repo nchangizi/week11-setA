0b6a2058a07c3f38d8c7481358407ccbb9a83987
Neda <nedachangizi@gmail.com>
2023-03-20 15:49:28 -0700

    update paragraphs

--- scripts/script.js
+++ scripts/script.js
@@ -16,11 +16,17 @@
 
 // greet("Neda");
 
+const radiusPara = document.querySelector("#radius");
+console.log(radiusPara);
+const resultPara = document.querySelector("#result");
+console.log(resultPara);
 // Write a function that takes one parameter as radius of a circle and return the circles area.
 function circleAreaCalculation(radius) {
   // check if radius is not a number and show an alert
   if (isNaN(radius)) {
-    alert("This is not a string");
+    // alert("This is not a number");
+    resultPara.textContent = "This is not a number";
+    // update the textContent or innerText property of the resultPara
   } else {
     let area = Math.PI * radius * radius;
     return area;
@@ -28,7 +34,13 @@ function circleAreaCalculation(radius) {
 }
 // Prompt user to enter a value.
 let userValue = prompt("Enter a number");
+// update the textContent or innerText property of the radiusPara
+radiusPara.textContent +=  userValue;
 // Call the area calculating function with the value entered by user.
 let result = circleAreaCalculation(userValue);
 // Show an alert message with both values e.g. “The area of a circle with radius 2 is 12.56”
-alert(`The area of a circle with radius ${userValue} is ${result.toFixed(2)}`);
+if (result !== undefined) {
+  // update the textContent or innerText property of the resultPara
+  resultPara.textContent = `The area of a circle with radius ${userValue} is ${result.toFixed(2)}`;
+  //   alert(`The area of a circle with radius ${userValue} is ${result}`);
+}
